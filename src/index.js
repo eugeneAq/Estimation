3c6579ed1c9bcae7003c235b9c2ba895166c90d8
eugeneAq <54995017+eugeneAq@users.noreply.github.com>
2019-09-08 04:46:01 +0800

    Createrd job details
    creater jobdetails,js
    projectDetails code to file
    to design pa

--- src/index.js
+++ src/index.js
@@ -2,6 +2,8 @@ import React from "react";
 import ReactDOM from "react-dom";
 import NavBar from "./navbar";
 import CartDetails from "./cartdetails";
+import JobDetails from "./jobdetails";
 
 ReactDOM.render(<NavBar />, document.querySelector("#navBar"));
 ReactDOM.render(<CartDetails />, document.querySelector("#cartDetails"));
+ReactDOM.render(<JobDetails />, document.querySelector("#jobDetails"));
